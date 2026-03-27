25034ebdd2493bcce85c936a4ea958e2e3ed1b37
Swapnil <swapnilsjadhav21@gmail.com>
2026-03-27 11:46:52 +0530

    API for complaint assignment strategy

--- src/modules/complaint_system/complaint_assignment_strategy/complaint_assignment_strategy.module.ts
+++ src/modules/complaint_system/complaint_assignment_strategy/complaint_assignment_strategy.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ComplaintAssignmentStrategy } from './entities/complaint_assignment_strategy.entity';
 import { ComplaintAssignmentStrategyService } from './complaint_assignment_strategy.service';
 import { ComplaintAssignmentStrategyController } from './complaint_assignment_strategy.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ComplaintAssignmentStrategy])],
+  controllers: [ComplaintAssignmentStrategyController],
   providers: [ComplaintAssignmentStrategyService],
-  controllers: [ComplaintAssignmentStrategyController]
+  exports: [ComplaintAssignmentStrategyService],
 })
 export class ComplaintAssignmentStrategyModule {}
