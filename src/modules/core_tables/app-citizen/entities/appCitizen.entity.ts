73193e1491bd27f02cda4678e7145403a67aa3f2
Aakash Devkar <aakashdevkar777@gmail.com>
2026-03-31 16:51:50 +0530

    minor change in complaint update status

--- src/modules/core_tables/app-citizen/entities/appCitizen.entity.ts
+++ src/modules/core_tables/app-citizen/entities/appCitizen.entity.ts
@@ -3,9 +3,15 @@ import { BaseTable } from "../../../../common/base.entity";
 
 @Entity('app_citizen') 
 export class AppCitizen extends BaseTable {
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', unique: true })
     mobile_no:string;
     
     @Column({ type: 'varchar', unique:true, nullable: true})
     name:string;
+
+    @Column({ type: 'varchar', nullable: true })
+    address: string;
+
+    @Column({ type: 'varchar', nullable: true })
+    email: string;
 }
