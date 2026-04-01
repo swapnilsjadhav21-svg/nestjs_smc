922de1f5b0f53bbea09dec8fc73e0d90b6ad9a61
Swapnil <swapnilsjadhav21@gmail.com>
2026-04-01 16:03:49 +0530

    added guards

--- src/modules/complaint_system/complaint_media/complaint_media.controller.ts
+++ src/modules/complaint_system/complaint_media/complaint_media.controller.ts
@@ -1,29 +1,31 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BaseCrudController } from 'src/common/crud/base-crud.controller';
 import { ComplaintMedia } from './entities/complaint_media.entity';
 import { CreateComplaintMediaDto } from './dto/create-complaint-media.dto';
 import { ComplaintMediaService } from './complaint_media.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('Complaint - Media')
 @Controller('complaint-media')
-export class ComplaintMediaController extends BaseCrudController<ComplaintMedia, CreateComplaintMediaDto> {
+export class ComplaintMediaController  {
   constructor(private readonly complaintMediaService: ComplaintMediaService) {
-    super(complaintMediaService);
   }
 
-  @Post(':id/media')
-  @ApiOperation({ summary: 'Attach media to a complaint' })
-  @ApiBody({ type: CreateComplaintMediaDto })
-  attachMedia(
-    @Param('id', ParseIntPipe) complaintId: number,
-    @Body() dto: CreateComplaintMediaDto,
-  ): Promise<ComplaintMedia> {
-    dto.complaint = { id: complaintId };
-    return this.complaintMediaService.create(dto);
-  }
+  // @Post(':id/media')
+  // @ApiOperation({ summary: 'Attach media to a complaint' })
+  // @ApiBody({ type: CreateComplaintMediaDto })
+  // attachMedia(
+  //   @Param('id', ParseIntPipe) complaintId: number,
+  //   @Body() dto: CreateComplaintMediaDto,
+  // ): Promise<ComplaintMedia> {
+  //   dto.complaint = { id: complaintId };
+  //   return this.complaintMediaService.create(dto);
+  // }
 
   @Get(':id/media')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @ApiOperation({ summary: 'Get all media for a complaint' })
   findByComplaintId(
     @Param('id', ParseIntPipe) complaintId: number,
@@ -32,8 +34,10 @@ export class ComplaintMediaController extends BaseCrudController<ComplaintMedia,
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
   @ApiOperation({ summary: 'Get all complaint media' })
-  override findAll(): Promise<ComplaintMedia[]> {
+  findAll(): Promise<ComplaintMedia[]> {
     return this.complaintMediaService.findAllWithRelations();
   }
 }
